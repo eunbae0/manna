454e27d89b0df4f5d01e379c31203d5dbce17ea1
EunBae Gong <geb9211@gmail.com>
2025-02-11 01:49:46 +0900

    feat: name, group screen에 스타일 적용

--- src/components/features/onboarding/screens/GroupStepScreen.tsx
+++ src/components/features/onboarding/screens/GroupStepScreen.tsx
@@ -1,34 +1,54 @@
+import { useState } from 'react';
+
+import { Button, ButtonText } from '#/components/ui/button';
 import Header from '@/components/common/Header';
 import { Text } from '#/components/ui/text';
-import { useOnboardingStore } from '@/store/onboarding';
-import { useState } from 'react';
-import { Animated, TextInput } from 'react-native';
-import { Button, ButtonText } from '#/components/ui/button';
 
-function GroupStepScreen() {
+import { useOnboardingStore } from '@/store/onboarding';
+import { Heading } from '#/components/ui/heading';
+import { Input, InputField } from '#/components/ui/input';
+import { VStack } from '#/components/ui/vstack';
+import { router } from 'expo-router';
+
+function NameStepScreen() {
 	const { setStep, updateUserData } = useOnboardingStore();
-	const [name, setName] = useState('');
+	const [group, setGroup] = useState('');
 
 	return (
-		<Animated.View>
-			<Header
-				label="이메일로 회원가입"
-				onPressBackButton={() => setStep('NAME')}
-			/>
-			<Text>소그룹 초대코드를 입력해주세요</Text>
-			<Text>초대된 그룹이 없다면 넘어갈 수 있어요.</Text>
-			<TextInput value={name} onChangeText={(text) => setName(text)} />
-
-			<Button
-				onPress={() => {
-					updateUserData({ name });
-					setStep('GROUP');
-				}}
-			>
-				<ButtonText>계속하기</ButtonText>
-			</Button>
-		</Animated.View>
+		<VStack>
+			<Header label="00로 회원가입" onPressBackButton={() => setStep('NAME')} />
+			<VStack space="4xl" className="px-4 mt-8">
+				<VStack space="3xl">
+					<VStack space="sm">
+						<Heading size="2xl">소그룹 초대코드를 입력해주세요</Heading>
+						<Text>초대된 그룹이 없다면 넘어갈 수 있어요.</Text>
+					</VStack>
+					<Input
+						variant="outline"
+						size="md"
+						isDisabled={false}
+						isInvalid={false}
+						isReadOnly={false}
+						className="rounded-2xl"
+					>
+						<InputField
+							value={group}
+							onChangeText={(text) => setGroup(text)}
+							placeholder="초대코드"
+						/>
+					</Input>
+				</VStack>
+				<Button
+					onPress={() => {
+						updateUserData({ group });
+						router.replace('/');
+					}}
+				>
+					<ButtonText>계속하기</ButtonText>
+				</Button>
+			</VStack>
+		</VStack>
 	);
 }
 
-export default GroupStepScreen;
+export default NameStepScreen;
