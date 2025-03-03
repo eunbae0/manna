75998f71d67bb73866387793783663298315e628
EunBae Gong <geb9211@gmail.com>
2025-03-04 03:11:49 +0900

    feat: 소그룹 메뉴 bottom sheet 추가

--- src/components/screens/home/HomeHeader.tsx
+++ src/components/screens/home/HomeHeader.tsx
@@ -1,16 +1,27 @@
+import { Pressable } from 'react-native';
 import { Button, ButtonIcon } from '#/components/ui/button';
 import { HStack } from '#/components/ui/hstack';
 import { Heading } from '#/components/ui/heading';
 import { Icon } from '#/components/ui/icon';
 import {
 	ChevronDown,
-	SettingsIcon,
 	MenuIcon,
 	UserRound,
+	HandHelping,
+	Library,
+	UserRoundPen,
+	X,
 } from 'lucide-react-native';
 import { Avatar, AvatarBadge, AvatarGroup } from '#/components/ui/avatar';
+import { Divider } from '#/components/ui/divider';
+import { Text } from '#/components/ui/text';
+import { VStack } from '#/components/ui/vstack';
+
+import { useBottomSheet } from '@/components/common/BottomSheetProvider';
 
 function HomeHeader() {
+	const { handleOpen, handleClose, BottomSheetContainer } = useBottomSheet();
+
 	return (
 		<HStack className="items-center justify-between pt-2 px-4">
 			<HStack space="xs" className="items-center">
@@ -34,7 +45,8 @@ function HomeHeader() {
 						<AvatarBadge className="bg-yellow-400" />
 					</Avatar>
 				</AvatarGroup>
-				<Button size="xl" variant="link">
+
+				<Button size="xl" variant="link" onPress={() => handleOpen()}>
 					<ButtonIcon
 						as={MenuIcon}
 						width={24}
@@ -43,6 +55,50 @@ function HomeHeader() {
 					/>
 				</Button>
 			</HStack>
+			<BottomSheetContainer>
+				<VStack className="w-full px-5 pt-2 pb-12">
+					<HStack className="justify-between items-center px-2">
+						<Heading size="xl">소그룹 메뉴</Heading>
+						<Button size="xl" variant="link">
+							<ButtonIcon as={X} />
+						</Button>
+					</HStack>
+					<VStack space="sm" className="w-full py-5">
+						<Pressable onPress={handleClose}>
+							<HStack space="md" className="items-center py-2">
+								<Icon
+									size="xl"
+									className="stroke-background-700"
+									as={HandHelping}
+								/>
+								<Text>기도 제목</Text>
+							</HStack>
+						</Pressable>
+						<Divider />
+						<Pressable onPress={handleClose}>
+							<HStack space="md" className="items-center py-2">
+								<Icon
+									size="xl"
+									className="stroke-background-700"
+									as={Library}
+								/>
+								<Text>나눔 기록</Text>
+							</HStack>
+						</Pressable>
+						<Divider />
+						<Pressable onPress={handleClose}>
+							<HStack space="md" className="items-center py-2">
+								<Icon
+									size="xl"
+									className="stroke-background-700"
+									as={UserRoundPen}
+								/>
+								<Text>소그룹원 관리하기</Text>
+							</HStack>
+						</Pressable>
+					</VStack>
+				</VStack>
+			</BottomSheetContainer>
 		</HStack>
 	);
 }
