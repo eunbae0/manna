e596a8d802cf595a57132a6ab344ec8fea077adc
EunBae Gong <geb9211@gmail.com>
2025-03-06 03:24:25 +0900

    feat: Header 컴포넌트 확장

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,31 +6,41 @@ import { Button, ButtonIcon } from '#/components/ui/button';
 import { ChevronLeftIcon } from '#/components/ui/icon';
 import { Divider } from '#/components/ui/divider';
 import { VStack } from '#/components/ui/vstack';
+import { cn } from '@/utils/cn';
 
 type Props = {
 	label: string;
 	onPressBackButton: () => void;
+	isLabelCentered?: boolean;
+	isDividerEnabled?: boolean;
 } & ViewProps;
 
-function Header({ label, onPressBackButton, children, ...props }: Props) {
+function Header({
+	label,
+	onPressBackButton,
+	isLabelCentered = false,
+	isDividerEnabled = false,
+	children,
+	...props
+}: Props) {
 	return (
 		<VStack space="xs">
-			<HStack space="md" className="ml-4 items-center" {...props}>
+			<HStack space="sm" className="pl-3 items-center" {...props}>
 				<Button size="xl" variant="link" onPress={onPressBackButton}>
-					<ButtonIcon as={ChevronLeftIcon} />
+					<ButtonIcon as={ChevronLeftIcon} className="w-8 h-8" />
 				</Button>
-				{/* <IconSymbol
-						size={28}
-						name="chevron.left"
-						color=""
-						// color={colors['neutral-900']}
-					/> */}
-				<Text bold={true} size="xl">
+				<Text
+					size="2xl"
+					className={cn(
+						'font-pretendard-semi-bold',
+						isLabelCentered ? 'absolute left-0 right-0 text-center' : '',
+					)}
+				>
 					{label}
 				</Text>
 				{children}
 			</HStack>
-			<Divider />
+			{isDividerEnabled && <Divider />}
 		</VStack>
 	);
 }
