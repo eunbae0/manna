a4cdee0c82617f03a6e41809e505c15a4e4bf1da
EunBae Gong <geb9211@gmail.com>
2025-03-14 03:54:08 +0900

    feat: header의 back button 기본 동작 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -7,10 +7,11 @@ import { ChevronLeftIcon } from '#/components/ui/icon';
 import { Divider } from '#/components/ui/divider';
 import { VStack } from '#/components/ui/vstack';
 import { cn } from '@/utils/cn';
+import { router } from 'expo-router';
 
 type Props = {
 	label: string;
-	onPressBackButton: () => void;
+	onPressBackButton?: () => void;
 	isLabelCentered?: boolean;
 	isDividerEnabled?: boolean;
 } & ViewProps;
@@ -21,23 +22,33 @@ function Header({
 	isLabelCentered = false,
 	isDividerEnabled = false,
 	children,
+	className,
 	...props
 }: Props) {
+	const defaultHandlePressBackButton = () => {
+		router.canGoBack() ? router.back() : router.dismissAll();
+	};
 	return (
 		<VStack space="xs">
-			<HStack space="sm" className="pl-3 items-center" {...props}>
-				<Button size="xl" variant="link" onPress={onPressBackButton}>
-					<ButtonIcon as={ChevronLeftIcon} className="w-8 h-8" />
-				</Button>
-				<Text
-					size="2xl"
-					className={cn(
-						'font-pretendard-semi-bold',
-						isLabelCentered ? 'absolute left-0 right-0 text-center' : '',
-					)}
-				>
-					{label}
-				</Text>
+			<HStack className={cn('w-full pl-3 items-center', className)} {...props}>
+				<HStack space="sm" className="items-center">
+					<Button
+						size="xl"
+						variant="link"
+						onPress={onPressBackButton || defaultHandlePressBackButton}
+					>
+						<ButtonIcon as={ChevronLeftIcon} className="w-8 h-8" />
+					</Button>
+					<Text
+						size="2xl"
+						className={cn(
+							'font-pretendard-semi-bold',
+							isLabelCentered ? 'absolute left-0 right-0 text-center' : '',
+						)}
+					>
+						{label}
+					</Text>
+				</HStack>
 				{children}
 			</HStack>
 			{isDividerEnabled && <Divider />}
