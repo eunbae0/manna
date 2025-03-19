8f89da4b59cc9a7333b2facb979d5bb52f145043
EunBae Gong <geb9211@gmail.com>
2025-03-20 00:39:37 +0900

    feat: KST Date 함수 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -17,7 +17,29 @@ export const parseKSTDate = (kstDateString: YYYYMMDD): Date => {
 
 	const date = new Date(Date.UTC(year, month - 1, day));
 
-	date.setUTCHours(0, 0, 0, 0);
-
 	return date;
+};
+
+/**
+ * Get the start of day in KST timezone (00:00:00.000)
+ * @param date Date object
+ * @returns Date object set to start of day in KST
+ */
+export const getStartOfDayKST = (date: Date): Date => {
+	const kstDate = new Date(date);
+
+	kstDate.setHours(0, 0, 0, 0);
+	return kstDate;
+};
+
+/**
+ * Get the end of day in KST timezone (23:59:59.999)
+ * @param date Date object
+ * @returns Date object set to end of day in KST
+ */
+export const getEndOfDayKST = (date: Date): Date => {
+	const kstDate = new Date(date);
+
+	kstDate.setHours(23, 59, 59, 999);
+	return kstDate;
 };
