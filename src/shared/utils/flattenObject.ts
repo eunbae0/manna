b805ed9a868a2aa774cce73070f898d8c0df4b5e
EunBae Gong <geb9211@gmail.com>
2025-04-03 05:07:43 +0900

    feat: dot notation 변환 함수 추가

---
+++ src/shared/utils/flattenObject.ts
@@ -0,0 +1,23 @@
+/**
+ * 중첩된 객체를 Firestore의 dot notation으로 변환하는 함수
+ * 예: { a: { b: 1 } } -> { 'a.b': 1 }
+ */
+export function flattenObject<T extends Record<string, unknown>>(obj: T, prefix = ''): Record<string, unknown> {
+	return Object.keys(obj).reduce<Record<string, unknown>>((acc, key) => {
+		const prefixedKey = prefix ? `${prefix}.${key}` : key;
+		const value = obj[key];
+		
+		if (
+			typeof value === 'object' && 
+			value !== null && 
+			!Array.isArray(value) &&
+			Object.keys(value as Record<string, unknown>).length > 0
+		) {
+			Object.assign(acc, flattenObject(value as Record<string, unknown>, prefixedKey));
+		} else {
+			acc[prefixedKey] = value;
+		}
+		
+		return acc;
+	}, {});
+}
