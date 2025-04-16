b78e3f29feb598230dd86b26bd212d344b4ef7b4
EunBae Gong <geb9211@gmail.com>
2025-04-16 17:33:41 +0900

    feat: 레이아웃 수정

--- src/features/onboarding/screens/EmailSignInStepScreen.tsx
+++ src/features/onboarding/screens/EmailSignInStepScreen.tsx
@@ -1,139 +1,121 @@
 import { useAuthStore } from '@/store/auth';
 import { router } from 'expo-router';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { Text } from '#/components/ui/text';
 import { Button, ButtonText } from '@/components/common/button';
 import Header from '@/components/common/Header';
-import { useOnboardingStore } from '@/store/onboarding';
 import { Heading } from '#/components/ui/heading';
 import { VStack } from '#/components/ui/vstack';
 import { Input, InputField } from '#/components/ui/input';
 import { Icon } from '#/components/ui/icon';
 import { Eye, EyeOff } from 'lucide-react-native';
-import { KeyboardAvoidingView } from '@/components/common/keyboard-view/KeyboardAvoidingView';
 import { KeyboardDismissView } from '@/components/common/keyboard-view/KeyboardDismissView';
-import { Pressable, type TextInput } from 'react-native';
+import { Pressable } from 'react-native';
 import { HStack } from '#/components/ui/hstack';
-import { sendPasswordResetEmail } from '@/api';
 
 function EmailSignInStepScreen() {
 	const { signIn } = useAuthStore();
-	const { setStep } = useOnboardingStore();
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [showPassword, setShowPassword] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
-	const inputRef = useRef<TextInput>(null);
-
-	// auto focus on input when screen is mounted
-	useEffect(() => {
-		setTimeout(() => {
-			inputRef.current?.focus();
-		}, 200);
-	}, []);
 
 	return (
 		<KeyboardDismissView>
-			<KeyboardAvoidingView>
-				<VStack className="h-full">
-					<Header
-						label="이메일로 로그인하기"
-						onPressBackButton={() =>
-							router.canGoBack() ? router.back() : router.push('/(auth)')
-						}
-					/>
-					<VStack className="flex-1 px-4 mt-8 justify-between" space="4xl">
-						<VStack space="4xl">
-							<VStack space="sm">
-								<Heading size="xl">계속하기 위해 로그인해주세요</Heading>
-								<Text size="lg">이메일과 비밀번호를 입력해주세요</Text>
-							</VStack>
-							<VStack space="lg" reversed={false}>
-								<VStack space="sm">
-									<Input
-										variant="rounded"
-										size="xl"
-										isDisabled={isLoading}
-										isInvalid={false}
-										isReadOnly={false}
-										className="rounded-2xl"
-									>
-										<InputField
-											//@ts-ignore
-											ref={inputRef}
-											value={email}
-											onChangeText={(text) => {
-												setEmail(text);
-											}}
-											placeholder="example@gmail.com"
-											autoCapitalize="none"
-											keyboardType="email-address"
-										/>
-									</Input>
-								</VStack>
-								<VStack space="sm">
-									<Input
-										variant="rounded"
-										size="xl"
-										isDisabled={isLoading}
-										isInvalid={false}
-										isReadOnly={false}
-										className="rounded-2xl"
-									>
-										<InputField
-											value={password}
-											onChangeText={(text) => {
-												setPassword(text);
-											}}
-											placeholder="비밀번호"
-											secureTextEntry={!showPassword}
-											autoCapitalize="none"
-										/>
-										<Pressable
-											onPress={() => setShowPassword(!showPassword)}
-											style={{ padding: 10 }}
-										>
-											<Icon as={showPassword ? EyeOff : Eye} size="lg" />
-										</Pressable>
-									</Input>
-								</VStack>
-							</VStack>
+			<VStack className="h-full">
+				<Header
+					label="이메일로 로그인하기"
+					onPressBackButton={() =>
+						router.canGoBack() ? router.back() : router.push('/(auth)')
+					}
+				/>
+				<VStack className="flex-1 px-4 mt-8 justify-between" space="4xl">
+					<VStack space="4xl">
+						<VStack space="sm">
+							<Heading size="xl">계속하기 위해 로그인해주세요</Heading>
+							<Text size="lg">이메일과 비밀번호를 입력해주세요</Text>
 						</VStack>
-						<VStack space="lg">
-							<HStack space="sm" className="w-full justify-center items-center">
-								<Text>비밀번호를 잊으셨나요?</Text>
-								<Pressable
-									onPress={() => router.push('/(auth)/forget-password')}
+						<VStack space="lg" reversed={false}>
+							<VStack space="sm">
+								<Input
+									variant="rounded"
+									size="xl"
+									isDisabled={isLoading}
+									isInvalid={false}
+									isReadOnly={false}
+									className="rounded-2xl"
 								>
-									<Text>비밀번호 찾기</Text>
-								</Pressable>
-							</HStack>
-							<Button
-								onPress={async () => {
-									if (email.length === 0 || password.length === 0) return;
-									setIsLoading(true);
-									try {
-										// 이메일/비밀번호로 로그인
-										await signIn('EMAIL', { email, password: password });
-										router.push('/(app)');
-									} catch (error) {
-										console.error('로그인 실패:', error);
-									} finally {
-										setIsLoading(false);
-									}
-								}}
-								disabled={
-									email.length === 0 || password.length === 0 || isLoading
-								}
-								size="xl"
-								className="rounded-full mb-5"
-							>
-								<ButtonText>{isLoading ? '로그인 중...' : '로그인'}</ButtonText>
-							</Button>
+									<InputField
+										value={email}
+										onChangeText={(text) => {
+											setEmail(text);
+										}}
+										placeholder="example@gmail.com"
+										autoCapitalize="none"
+										keyboardType="email-address"
+									/>
+								</Input>
+							</VStack>
+							<VStack space="sm">
+								<Input
+									variant="rounded"
+									size="xl"
+									isDisabled={isLoading}
+									isInvalid={false}
+									isReadOnly={false}
+									className="rounded-2xl"
+								>
+									<InputField
+										value={password}
+										onChangeText={(text) => {
+											setPassword(text);
+										}}
+										placeholder="비밀번호"
+										secureTextEntry={!showPassword}
+										autoCapitalize="none"
+									/>
+									<Pressable
+										onPress={() => setShowPassword(!showPassword)}
+										style={{ padding: 10 }}
+									>
+										<Icon as={showPassword ? EyeOff : Eye} size="lg" />
+									</Pressable>
+								</Input>
+							</VStack>
 						</VStack>
 					</VStack>
+					<VStack space="lg">
+						<HStack space="sm" className="w-full justify-center items-center">
+							<Text>비밀번호를 잊으셨나요?</Text>
+							<Pressable onPress={() => router.push('/(auth)/forget-password')}>
+								<Text>비밀번호 찾기</Text>
+							</Pressable>
+						</HStack>
+						<Button
+							onPress={async () => {
+								if (email.length === 0 || password.length === 0) return;
+								setIsLoading(true);
+								try {
+									// 이메일/비밀번호로 로그인
+									await signIn('EMAIL', { email, password: password });
+									router.push('/(app)');
+								} catch (error) {
+									console.error('로그인 실패:', error);
+								} finally {
+									setIsLoading(false);
+								}
+							}}
+							disabled={
+								email.length === 0 || password.length === 0 || isLoading
+							}
+							size="xl"
+							rounded
+						>
+							<ButtonText>{isLoading ? '로그인 중...' : '로그인'}</ButtonText>
+						</Button>
+					</VStack>
 				</VStack>
-			</KeyboardAvoidingView>
+			</VStack>
 		</KeyboardDismissView>
 	);
 }
