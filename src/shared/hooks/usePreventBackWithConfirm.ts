24bb31e6186371618e26398fe1b3f4faaaa834d3
EunBae Gong <geb9211@gmail.com>
2025-04-23 02:46:50 +0900

    fix: iOS에서 쓸어서 뒤로가기시에도 prevent하도록 수정

--- src/shared/hooks/usePreventBackWithConfirm.ts
+++ src/shared/hooks/usePreventBackWithConfirm.ts
@@ -82,7 +82,7 @@ export function usePreventBackWithConfirm({
 	usePreventRemove(condition, ({ data }) => {
 		// FIX: iOS에서 뒤로가기시 action.type이 'GO_BACK'이 인식되지 않아 뒤로가기가 수행됨
 		console.log(data);
-		if (data.action.type !== 'GO_BACK') {
+		if (data.action.type !== 'GO_BACK' && data.action.type !== 'POP') {
 			navigation.dispatch(data.action);
 			return;
 		}
