c6f492695a4f59280e553db28d17682637407a71
EunBae Gong <geb9211@gmail.com>
2025-04-22 12:24:01 +0900

    feat: bottom tabs를 expo-router/ui 컴포넌트로 교체

--- src/app/(app)/(tabs)/_layout.tsx
+++ src/app/(app)/(tabs)/_layout.tsx
@@ -1,64 +1,152 @@
 import React from 'react';
-import { Tabs } from 'expo-router';
+import {
+	Tabs,
+	TabList,
+	TabTrigger,
+	TabSlot,
+	type TabTriggerSlotProps,
+} from 'expo-router/ui';
 
-import { HapticTab } from '@/components/common/HapticTab';
-import { Colors } from '@/constants/Colors';
 import { Icon } from '#/components/ui/icon';
-import { Bell, HouseIcon, NotebookPen, Ellipsis } from 'lucide-react-native';
+import {
+	Bell,
+	HouseIcon,
+	NotebookPen,
+	Ellipsis,
+	type LucideIcon,
+} from 'lucide-react-native';
+import * as Haptics from 'expo-haptics';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Text } from '#/components/ui/text';
+import { VStack } from '#/components/ui/vstack';
+import { Pressable, type View } from 'react-native';
+import { cn } from '@/shared/utils/cn';
+import Animated, {
+	useSharedValue,
+	useAnimatedStyle,
+	withTiming,
+	Easing,
+} from 'react-native-reanimated';
 
 export default function TabLayout() {
 	const colorScheme = useColorScheme();
 	return (
-		<Tabs
-			screenOptions={({ route }) => ({
-				tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-				headerShown: false,
-				tabBarButton: HapticTab,
-				tabBarStyle: [
-					{
-						paddingLeft: 14,
-						paddingRight: 14,
-					},
-				],
-				sceneStyle: { backgroundColor: 'transparent' },
-			})}
-		>
-			<Tabs.Screen
-				name="index"
-				options={{
-					title: '나의 소그룹',
-					tabBarIcon: ({ color }) => (
-						<Icon size="xl" as={HouseIcon} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="note"
-				options={{
-					title: '설교 노트',
-					tabBarIcon: ({ color }) => (
-						<Icon size="xl" as={NotebookPen} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="notification"
-				options={{
-					title: '알림',
-					tabBarIcon: ({ color }) => <Icon size="xl" as={Bell} color={color} />,
-				}}
-			/>
-			<Tabs.Screen
-				name="more"
-				options={{
-					title: '더보기',
-					tabBarIcon: ({ color }) => (
-						<Icon size="xl" as={Ellipsis} color={color} />
-					),
-				}}
-			/>
+		<Tabs>
+			<TabSlot />
+			<TabList className="px-3 border-t border-gray-200 items-center justify-center">
+				<TabTrigger
+					name="index"
+					href="/"
+					asChild
+					style={{ flex: 1, justifyContent: 'center' }}
+				>
+					<CustomTabButton icon={HouseIcon}>나의 소그룹</CustomTabButton>
+				</TabTrigger>
+				<TabTrigger
+					name="note"
+					href="/note"
+					asChild
+					style={{ flex: 1, justifyContent: 'center' }}
+				>
+					<CustomTabButton icon={NotebookPen}>설교 노트</CustomTabButton>
+				</TabTrigger>
+				<TabTrigger
+					name="notification"
+					href="/notification"
+					asChild
+					style={{ flex: 1, justifyContent: 'center' }}
+				>
+					<CustomTabButton icon={Bell}>알림</CustomTabButton>
+				</TabTrigger>
+				<TabTrigger
+					name="more"
+					href="/more"
+					asChild
+					style={{ flex: 1, justifyContent: 'center' }}
+				>
+					<CustomTabButton icon={Ellipsis}>더보기</CustomTabButton>
+				</TabTrigger>
+			</TabList>
 		</Tabs>
 	);
 }
+
+interface CustomTabButtonProps
+	extends React.PropsWithChildren,
+		TabTriggerSlotProps {
+	icon: LucideIcon;
+}
+
+export const CustomTabButton = React.forwardRef<View, CustomTabButtonProps>(
+	(props, ref) => {
+		// 애니메이션을 위한 공유 값 생성
+		const scale = useSharedValue(1);
+		const backgroundColor = useSharedValue(0);
+
+		// 애니메이션 스타일 정의
+		const animatedStyle = useAnimatedStyle(() => {
+			return {
+				transform: [{ scale: scale.value }],
+				backgroundColor: `rgba(54, 35, 3, ${backgroundColor.value * 0.04})`,
+				borderRadius: 8,
+				paddingTop: 8,
+				paddingBottom: 6,
+			};
+		});
+
+		return (
+			<Pressable
+				ref={ref}
+				{...props}
+				onPressIn={(ev) => {
+					scale.value = withTiming(0.96, {
+						duration: 150,
+						easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+					});
+					backgroundColor.value = withTiming(1, {
+						duration: 150,
+						easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+					});
+
+					if (process.env.EXPO_OS === 'ios') {
+						Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+					}
+					props.onPressIn?.(ev);
+				}}
+				onPressOut={() => {
+					scale.value = withTiming(1, {
+						duration: 150,
+						easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+					});
+					backgroundColor.value = withTiming(0, {
+						duration: 150,
+						easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+					});
+				}}
+			>
+				<Animated.View style={animatedStyle}>
+					<VStack className="items-center gap-[1px]">
+						<Icon
+							size="xl"
+							as={props.icon}
+							className={cn(
+								props.isFocused
+									? 'stroke-primary-500'
+									: 'stroke-typography-500',
+							)}
+						/>
+						<Text
+							size="xs"
+							className={cn(
+								props.isFocused ? 'text-primary-500' : 'text-typography-500',
+							)}
+						>
+							{props.children}
+						</Text>
+					</VStack>
+				</Animated.View>
+			</Pressable>
+		);
+	},
+);
