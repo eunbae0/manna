9b7c61a9c5dc6c0383eb20b4287afcab0031178c
EunBae Gong <geb9211@gmail.com>
2025-04-25 18:53:36 +0900

    feat: ListItem에 new 표시 ui 추가

--- src/shared/components/ListItem/index.tsx
+++ src/shared/components/ListItem/index.tsx
@@ -9,10 +9,12 @@ export const ListItem = ({
 	icon,
 	label,
 	onPress,
+	isNew = false,
 }: {
 	icon: LucideIcon;
 	label: string;
 	onPress?: PressableProps['onPress'];
+	isNew?: boolean;
 }) => {
 	return (
 		<AnimatedPressable onPress={onPress}>
@@ -20,6 +22,11 @@ export const ListItem = ({
 				<HStack space="md" className="items-center">
 					<Icon as={icon} size="md" />
 					<Text size="lg">{label}</Text>
+					{isNew && (
+						<Text size="md" className="text-yellow-500">
+							NEW!
+						</Text>
+					)}
 				</HStack>
 				<Icon as={ChevronRight} size="lg" />
 			</HStack>
