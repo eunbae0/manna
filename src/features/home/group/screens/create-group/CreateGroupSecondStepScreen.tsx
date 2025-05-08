829311eb9e64360d1da96f2554838e038f7acf03
EunBae Gong <geb9211@gmail.com>
2025-05-08 14:11:40 +0900

    feat: 그룹 생성시 QR 코드 보기 추가

--- src/features/home/group/screens/create-group/CreateGroupSecondStepScreen.tsx
+++ src/features/home/group/screens/create-group/CreateGroupSecondStepScreen.tsx
@@ -2,13 +2,15 @@ import { VStack } from '#/components/ui/vstack';
 import Header from '@/components/common/Header';
 import { Heading } from '@/shared/components/heading';
 import { Text } from '#/components/ui/text';
-import { Button, ButtonText } from '@/components/common/button';
+import { Button, ButtonIcon, ButtonText } from '@/components/common/button';
 import { Box } from '#/components/ui/box';
 import { useOnboardingStore } from '@/store/onboarding';
 import type { ClientGroup } from '@/api/group/types';
 import { router } from 'expo-router';
 import { useAuthStore } from '@/store/auth';
 import { useCopyInviteCode } from '@/shared/hooks/useCopyInviteCode';
+import { Copy, QrCode } from 'lucide-react-native';
+import { HStack } from '#/components/ui/hstack';
 
 type Props = {
 	group: ClientGroup | null;
@@ -44,15 +46,37 @@ export default function CreateGroupSecondStepScreen({ group }: Props) {
 								코드를 공유해 친구를 초대해보세요
 							</Text>
 						</VStack>
-						<VStack space="sm">
+						<VStack space="md">
 							<Box className="w-full rounded-2xl bg-background-0 border-primary-600 border-2 p-4">
 								<Text className="text-3xl font-pretendard-semi-bold text-primary-900  text-center">
 									{group?.inviteCode ?? ''}
 								</Text>
 							</Box>
-							<Button variant="link" onPress={copyInviteCode}>
-								<ButtonText>코드 복사하기</ButtonText>
-							</Button>
+							<HStack space="sm">
+								<Button
+									variant="outline"
+									onPress={copyInviteCode}
+									className="flex-1"
+								>
+									<ButtonText>코드 복사하기</ButtonText>
+									<ButtonIcon as={Copy} />
+								</Button>
+								<Button
+									variant="outline"
+									onPress={() =>
+										router.push({
+											pathname: '/(app)/inviteQrCodeModal',
+											params: {
+												inviteCode: group?.inviteCode,
+											},
+										})
+									}
+									className="flex-1"
+								>
+									<ButtonText>QR코드 보기</ButtonText>
+									<ButtonIcon as={QrCode} />
+								</Button>
+							</HStack>
 						</VStack>
 					</VStack>
 				</VStack>
