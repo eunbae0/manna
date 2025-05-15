00bb09cd9a518c31c8d11986a7b19e6de6b4402c
EunBae Gong <geb9211@gmail.com>
2025-05-16 05:39:34 +0900

    feat: board 알림 기능 구현

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -155,21 +155,25 @@ exports.fellowshipNotification = onDocumentCreated(
 
 		const group = await event.data?.ref.parent.parent?.get();
 
-		const members: { id: string }[] = [];
+		const members = new Set<string>();
 		const { id: groupId } = group?.data() as {
 			id: string;
 		};
 
+		const fellowshipMemberIds = new Set(
+			fellowshipMembers.map((member) => member.id),
+		);
+
 		const membersRef = await group?.ref.collection('members').get();
+
 		for (const memberDoc of membersRef?.docs || []) {
 			const member = memberDoc.data() as { id: string };
-			members.push(member);
+			if (!fellowshipMemberIds.has(member.id)) continue;
+			if (member.id === senderId) continue;
+			members.add(member.id);
 		}
 
-		const otherMembers = members.filter((member) => member.id !== senderId);
-
-		for (const member of otherMembers) {
-			const currentMemberId = member.id;
+		for (const currentMemberId of members) {
 			const userDoc = await firestore()
 				.collection('users')
 				.doc(currentMemberId)
@@ -260,8 +264,273 @@ exports.fellowshipNotification = onDocumentCreated(
 						log(`Notification sent and saved successfully: ${messageId}`);
 					} catch (error) {
 						warn(`Notification failed to send: ${error}`);
-						await cleanupToken(error, member.id, token);
+						await cleanupToken(error, currentMemberId, token);
 					}
+				}
+			}
+		}
+	},
+);
+
+exports.boardNewPostNotification = onDocumentCreated(
+	'/groups/{documentId}/posts/{documentId}',
+	async (event) => {
+		const {
+			id: boardId,
+			title: boardTitle,
+			groupId,
+			author: { id: senderId },
+		} = event.data?.data() as {
+			id: string;
+			title: string;
+			groupId: string;
+			author: { id: string };
+		};
+
+		const group = await event.data?.ref.parent.parent?.get();
+
+		let senderDisplayName = '';
+		const members = new Set<string>();
+
+		const membersRef = await group?.ref.collection('members').get();
+
+		for (const memberDoc of membersRef?.docs || []) {
+			const member = memberDoc.data() as { id: string; displayName: string };
+			if (member.id === senderId) {
+				senderDisplayName = member.displayName;
+				continue;
+			}
+			members.add(member.id);
+		}
+
+		for (const currentMemberId of members) {
+			const userDoc = await firestore()
+				.collection('users')
+				.doc(currentMemberId)
+				.get();
+			if (!userDoc.exists) continue;
+
+			const user = userDoc.data() as {
+				fcmTokens?: string[];
+			};
+
+			const userGroupDocs = await userDoc.ref.collection('groups').get();
+			const userGroups: {
+				groupId: string;
+				notificationPreferences: {
+					fellowship: boolean;
+					prayerRequest: boolean;
+					board?: {
+						activity: boolean;
+						newPost: boolean;
+					};
+				};
+			}[] = [];
+			for (const groupDoc of userGroupDocs.docs) {
+				const data = groupDoc.data() as {
+					groupId: string;
+					notificationPreferences: {
+						fellowship: boolean;
+						prayerRequest: boolean;
+						board?: {
+							activity: boolean;
+							newPost: boolean;
+						};
+					};
+				};
+				userGroups.push(data);
+			}
+
+			const userGroup = userGroups.find((group) => group.groupId === groupId);
+
+			const groupDoc = await firestore()
+				.collection('groups')
+				.doc(groupId)
+				.get();
+			if (!groupDoc.exists) continue;
+
+			const { groupName } = groupDoc.data() as {
+				groupName: string;
+			};
+
+			if (userGroup?.notificationPreferences?.board?.newPost !== false) {
+				const tokens = user.fcmTokens;
+				if (!tokens) continue;
+
+				const payload = {
+					notification: {
+						title: `${senderDisplayName} 님이 새 게시글을 등록했어요`,
+						body: boardTitle,
+					},
+					data: {
+						screen: `/(app)/(board)/${boardId}`,
+						groupId,
+						groupName,
+					},
+				};
+
+				let isNotificationSentwithSaveFirestore = false;
+				const messageId = uuidv4();
+
+				for (const token of tokens) {
+					try {
+						// send notification
+						await messaging().send({
+							token,
+							...payload,
+						});
+						if (isNotificationSentwithSaveFirestore) continue;
+
+						// save notification
+						await firestore()
+							.collection('users')
+							.doc(currentMemberId)
+							.collection('notifications')
+							.doc(messageId)
+							.set({
+								...payload.notification,
+								...payload.data,
+								metadata: {
+									groupId,
+									boardId,
+									senderId,
+								},
+								isRead: false,
+								timestamp: firestore.FieldValue.serverTimestamp(),
+							});
+						isNotificationSentwithSaveFirestore = true;
+						log(`Notification sent and saved successfully: ${messageId}`);
+					} catch (error) {
+						warn(`Notification failed to send: ${error}`);
+						await cleanupToken(error, currentMemberId, token);
+					}
+				}
+			}
+		}
+	},
+);
+
+exports.boardActivityNotification = onDocumentCreated(
+	'/groups/{documentId}/posts/{documentId}/comments/{documentId}',
+	async (event) => {
+		const {
+			content: boardContent,
+			groupId,
+			author: { id: senderId },
+		} = event.data?.data() as {
+			content: string;
+			groupId: string;
+			author: { id: string };
+		};
+
+		const post = await event.data?.ref.parent.parent?.get();
+
+		const postDoc = await post?.ref.get();
+		if (!postDoc?.exists) return;
+
+		const postData = postDoc.data() as { author: { id: string }; id: string };
+
+		const boardId = postData.id;
+		const receiverId = postData.author.id;
+
+		const receiverDoc = await firestore()
+			.collection('users')
+			.doc(receiverId)
+			.get();
+		if (!receiverDoc.exists) return;
+
+		const receiver = receiverDoc.data() as {
+			fcmTokens?: string[];
+		};
+
+		const receiverGroupDocs = await receiverDoc.ref.collection('groups').get();
+		const receiverGroups: {
+			groupId: string;
+			notificationPreferences: {
+				fellowship: boolean;
+				prayerRequest: boolean;
+				board?: {
+					activity: boolean;
+					newPost: boolean;
+				};
+			};
+		}[] = [];
+		for (const groupDoc of receiverGroupDocs.docs) {
+			const data = groupDoc.data() as {
+				groupId: string;
+				notificationPreferences: {
+					fellowship: boolean;
+					prayerRequest: boolean;
+					board?: {
+						activity: boolean;
+						newPost: boolean;
+					};
+				};
+			};
+			receiverGroups.push(data);
+		}
+
+		const receiverGroup = receiverGroups.find(
+			(group) => group.groupId === groupId,
+		);
+
+		const groupDoc = await firestore().collection('groups').doc(groupId).get();
+		if (!groupDoc.exists) return;
+
+		const { groupName } = groupDoc.data() as {
+			groupName: string;
+		};
+
+		if (receiverGroup?.notificationPreferences?.board?.activity !== false) {
+			const tokens = receiver.fcmTokens;
+			if (!tokens) return;
+
+			const payload = {
+				notification: {
+					title: '게시글에 댓글이 달렸어요',
+					body: boardContent,
+				},
+				data: {
+					screen: `/(app)/(board)/${boardId}`,
+					groupId,
+					groupName,
+				},
+			};
+
+			let isNotificationSentwithSaveFirestore = false;
+			const messageId = uuidv4();
+
+			for (const token of tokens) {
+				try {
+					// send notification
+					await messaging().send({
+						token,
+						...payload,
+					});
+					if (isNotificationSentwithSaveFirestore) continue;
+
+					// save notification
+					await firestore()
+						.collection('users')
+						.doc(receiverId)
+						.collection('notifications')
+						.doc(messageId)
+						.set({
+							...payload.notification,
+							...payload.data,
+							metadata: {
+								groupId,
+								boardId,
+								senderId,
+							},
+							isRead: false,
+							timestamp: firestore.FieldValue.serverTimestamp(),
+						});
+					isNotificationSentwithSaveFirestore = true;
+					log(`Notification sent and saved successfully: ${messageId}`);
+				} catch (error) {
+					warn(`Notification failed to send: ${error}`);
+					await cleanupToken(error, receiverId, token);
 				}
 			}
 		}
