c7dc3d5cd7f07cf45a0807a3a73aa72deb640e92
EunBae Gong <geb9211@gmail.com>
2025-05-24 04:26:09 +0900

    chore: update version 1.1.1

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
 		displayName: IS_DEVELOPMENT ? '소그룹 (개발)' : '소그룹',
 		slug: 'so-group',
 		scheme: ['sogroup-development', 'sogroup'],
-		version: '1.1.0',
+		version: '1.1.1',
 		orientation: 'portrait',
 		icon: './assets/images/icons/icon.png',
 		userInterfaceStyle: 'automatic',
@@ -22,7 +22,7 @@ export default {
 				CADisableMinimumFrameDurationOnPhone: true,
 				UIBackgroundModes: ['remote-notification'],
 				CFBundleDisplayName: IS_DEVELOPMENT ? '소그룹 (개발)' : '소그룹',
-				CFBundleVersion: '1.1.0',
+				CFBundleVersion: '1.1.1',
 			},
 			entitlements: {
 				'com.apple.developer.applesignin': ['Default'],
@@ -33,10 +33,10 @@ export default {
 				? './assets/google/development/GoogleService-Info.plist'
 				: './assets/google/GoogleService-Info.plist',
 			appleTeamId: 'AQ62DT56AM',
-			version: '1.1.0',
+			version: '1.1.1',
 		},
 		android: {
-			versionCode: 11000,
+			versionCode: 11100,
 			adaptiveIcon: {
 				foregroundImage: './assets/images/icons/android_app_icon.png',
 				backgroundColor: '#FEF8EF',
