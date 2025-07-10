b43802d3dc8ea848aefbe8b8248cb98846f38d36
EunBae Gong <geb9211@gmail.com>
2025-07-10 18:04:35 +0900

    feat: image placeholder blurHash 수정

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,5 +1,4 @@
-export const IMAGE_BLUR_HASH =
-	'|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+export const IMAGE_BLUR_HASH = 'L1RC[6-;fQ-;~qj[fQj[fQfQfQfQ';
 
 export const DELETED_MEMBER_DISPLAY_NAME = '알수없음';
 
