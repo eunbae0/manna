a7b3250ebc449fb6ca1f9bb32eedd34ac2d0bd81
EunBae Gong <geb9211@gmail.com>
2025-07-12 11:47:31 +0900

    feat: 메인 헤더에 안읽은 알림 표시 추가

--- src/features/home/components/HomeHeader.tsx
+++ src/features/home/components/HomeHeader.tsx
@@ -1,13 +1,19 @@
 import { HStack } from "#/components/ui/hstack";
 import { Button, ButtonIcon } from "@/components/common/button";
+import { useNotifications } from "@/features/notification/hooks/useNotifications";
 import { Image } from 'expo-image';
 import { router } from "expo-router";
 import { Bell } from "lucide-react-native";
+import { View } from "react-native";
 
 export function HomeHeader() {
   const handlePressNotification = () => {
     router.push('/(app)/notification')
   }
+
+  const { unreadCount } = useNotifications()
+
+  const isUnread = unreadCount > 0
 
   return (
     <HStack className="pl-5 pr-3 pb-3 w-full items-center justify-between">
@@ -16,9 +22,12 @@ export function HomeHeader() {
         style={{ width: 40, height: 40 }}
         contentFit="contain"
       />
-      <Button variant="icon" size="lg" onPress={handlePressNotification}>
-        <ButtonIcon as={Bell} />
-      </Button>
+      <View>
+        <Button variant="icon" size="lg" onPress={handlePressNotification}>
+          <ButtonIcon as={Bell} />
+        </Button>
+        {isUnread && <View className="absolute top-2 right-2 w-[5px] h-[5px] rounded-full bg-red-500" />}
+      </View>
     </HStack>
   )
 }
