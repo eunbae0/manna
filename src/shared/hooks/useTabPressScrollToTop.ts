174dd8697eb751e5d84a402283b23ae13e33dc50
EunBae Gong <geb9211@gmail.com>
2025-07-16 00:37:41 +0900

    feat: 탭을 다시 선택했을 때만 scrollToTop을 실행하도록 개선

--- src/shared/hooks/useTabPressScrollToTop.ts
+++ src/shared/hooks/useTabPressScrollToTop.ts
@@ -1,14 +1,16 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import type { FlashList } from '@shopify/flash-list';
 import { useEffect } from 'react';
 import type { FlatList, ScrollView } from 'react-native';
+import { useNavigation } from 'expo-router';
+import type { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
 export function useTabPressScrollToTop<
 	T extends ScrollView | FlatList<any> | FlashList<any>,
 >() {
 	const ref = useRef<T>(null);
-
-	const scrollToTop = () => {
+	const navigation = useNavigation<BottomTabNavigationProp<any>>();
+	const scrollToTop = useCallback(() => {
 		if (!ref.current) return;
 		if ('scrollToOffset' in ref.current) {
 			(ref.current as FlatList<any> | FlashList<any>).scrollToOffset({
@@ -18,11 +20,20 @@ export function useTabPressScrollToTop<
 		} else {
 			(ref.current as ScrollView).scrollTo({ animated: true, y: 0 });
 		}
-	};
+	}, []);
 
 	useEffect(() => {
-		scrollToTop();
-	});
+		const unsubscribe = navigation.addListener('tabPress', (e) => {
+			const state = navigation.getState();
+			const isCurrentTab = state.history
+				.map((i) => i.key)
+				.includes(e.target as string);
 
+			if (!isCurrentTab) return;
+			scrollToTop();
+		});
+
+		return () => unsubscribe();
+	}, [navigation, scrollToTop]);
 	return ref;
 }
