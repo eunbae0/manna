288cb4ea0461061acbe938e976ba00cb83348b68
EunBae Gong <geb9211@gmail.com>
2025-07-16 17:25:06 +0900

    feat: 나눔 답변 작성 화면에서 KeyboardDIssmissView 추가

--- src/app/(app)/(fellowship)/[id]/answer.tsx
+++ src/app/(app)/(fellowship)/[id]/answer.tsx
@@ -9,6 +9,7 @@ import { KeyboardAwareScrollView } from '@/shared/components/KeyboardAwareScroll
 import { KeyboardToolbar } from '@/shared/components/KeyboardToolbar';
 import { AnswerField } from '@/features/fellowship/components/answer/AnswerField';
 import Divider from '@/shared/components/divider';
+import { KeyboardDismissView } from '@/components/common/keyboard-view/KeyboardDismissView';
 
 export default function AnswerScreen() {
   const { id, contentType, answerId } = useLocalSearchParams<{
@@ -73,35 +74,37 @@ export default function AnswerScreen() {
         <VStack space="xs">
           <Header />
           <KeyboardAwareScrollView showsVerticalScrollIndicator={false} keyboardShouldPersistTaps="always">
-            {!isPrayerRequest && (
-              <>
-                <VStack space="md" className="px-4 pt-2 pb-4">
-                  <Heading size="2xl">현재 질문</Heading>
-                  <Text size="lg">
-                    {fellowship.content.categories[contentType].items[answerId].title}
-                  </Text>
-                </VStack>
+            <KeyboardDismissView>
+              {!isPrayerRequest && (
+                <>
+                  <VStack space="md" className="px-4 pt-2 pb-4">
+                    <Heading size="2xl">현재 질문</Heading>
+                    <Text size="lg">
+                      {fellowship.content.categories[contentType].items[answerId].title}
+                    </Text>
+                  </VStack>
 
-                <Divider size="lg" className="my-2" />
-              </>
-            )}
-            <VStack space="4xl" className="mx-4 mt-2 mb-32">
-              <VStack space="xl">
-                <VStack space="2xl" className="pt-2">
-                  <Heading size="2xl">{isPrayerRequest ? '기도제목 작성' : '나눔 답변 작성'}</Heading>
-                  <VStack className="gap-10">
-                    {fellowship.info.participants.map((member) => (
-                      <AnswerField
-                        key={member.id}
-                        member={member}
-                        answer={fellowship.content.categories[contentType].items[answerId].answers[member.id] || ''}
-                        updateAnswer={updateAnswer}
-                      />
-                    ))}
+                  <Divider size="lg" className="my-2" />
+                </>
+              )}
+              <VStack space="4xl" className="mx-4 mt-2 mb-32">
+                <VStack space="xl">
+                  <VStack space="2xl" className="pt-2">
+                    <Heading size="2xl">{isPrayerRequest ? '기도제목 작성' : '나눔 답변 작성'}</Heading>
+                    <VStack className="gap-10">
+                      {fellowship.info.participants.map((member) => (
+                        <AnswerField
+                          key={member.id}
+                          member={member}
+                          answer={fellowship.content.categories[contentType].items[answerId].answers[member.id] || ''}
+                          updateAnswer={updateAnswer}
+                        />
+                      ))}
+                    </VStack>
                   </VStack>
                 </VStack>
               </VStack>
-            </VStack>
+            </KeyboardDismissView>
           </KeyboardAwareScrollView>
         </VStack>
       </SafeAreaView>
