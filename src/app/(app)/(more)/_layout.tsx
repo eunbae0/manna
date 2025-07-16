268579991bf08b1779a1179867c19c22f2bc77cf
EunBae Gong <geb9211@gmail.com>
2025-07-17 04:18:40 +0900

    feat: 사용하지 않는 stack routing 삭제

--- src/app/(app)/(more)/_layout.tsx
+++ src/app/(app)/(more)/_layout.tsx
@@ -9,9 +9,7 @@ export default function HomeLayout() {
 			}}
 		>
 			<Stack.Screen name="screen" />
-			<Stack.Screen name="notification" />
 			<Stack.Screen name="account" />
-			<Stack.Screen name="policy" />
 			<Stack.Screen name="support" />
 			<Stack.Screen name="(feedback)" />
 			<Stack.Screen name="notice" />
